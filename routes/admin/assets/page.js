5a6e40fde7592252c150e4f1143ee576c2d44039
Jawad <jawad.azizi1398@gmail.com>
2023-08-12 18:10:57 +0430

    update asset added

--- routes/admin/assets/page.js
+++ routes/admin/assets/page.js
@@ -15,8 +15,8 @@ import {
   Input,
 } from "@ulibs/ui";
 import { runAction } from "../../../utils/ui.js";
-import { copyFileSync, rmSync, cpSync, renameSync, copyFile, cp, } from 'fs'
-import { basename, extname, resolve, join } from 'path'
+import { copyFileSync, rmSync} from 'fs'
+import { basename } from 'path'
 
 export default function () {
   return OffCanvas(); 
@@ -45,17 +45,56 @@ export  async function getAssets({ctx, body}){
       },
     };
 }
+export async function getAsset({ ctx, body }) {
+  console.log('getasset body' , body)
+  if (!body?.id) {
+    return { body: { success: false, message: "missing asset id" } };
+  }
+  const asset = await ctx
+    .table("assets")
+    .query({ where: { id: body.id } })
+    .then((res) => res.data[0]);
+  console.log('asset=====>: ', asset)
+  const result = updateModal(
+    asset,
+    {
+      image: View({
+        tag: "img",
+        src: asset.url,
+        alt: asset.alt,
+        width: "500px",
+      }),
+      audeo: View({ tag: "audeo", width: "500px" }, [
+        View({ tag: "source", src: asset.url }),
+        View("View"),
+      ]),
+      vidio: View({ tag: "video", width: "500px" }, [
+        View({ tag: "source", src: asset.url }),
+        View("View"),
+      ]),
+    }[asset.type]
+  );
+
+  return {
+    body: {
+      success: true,
+      asset,
+      view: result.toString(),
+    },
+  };
+}
 
 export  async function upload({ctx, body, files}){
-  const supporteTypes = ['image', 'video', 'audeo']
+  const supportedTypes = ['image', 'video', 'audeo']
   const file = files.file
   const path = file.path.split('\\').join("/")
   const type = file.mimetype.split('/')[0]
-  if(!supporteTypes.includes(type)){
+  if(!supportedTypes.includes(type)){
     throw new Error('Unsupported file format, unable to save this file type: type == '+ type)
   }
   console.log('object:>>>+>>', files, typeof file.path)
   
+  //from temp folder to project folder
   copyFileSync(path, `./assets/${type}s/${basename(file.path)}`);
   rmSync(path)
   
@@ -69,7 +108,7 @@ export  async function upload({ctx, body, files}){
       width: "",
       height: "",
     };
-      await ctx.table('assets').insert(asset)
+    await ctx.table('assets').insert(asset)
     return {
         body: {success: true},
     }
@@ -84,14 +123,54 @@ export async function remove({ctx, body, files}){
     body:{success: true}
   }
 }
-``
+
 export async function update({ctx, body, files}){
   console.log('update asest action body: ', body)
 
-  ctx.table('assets').upload(body.asset)
+  ctx.table('assets').update(body.id, body)
+  
   return {
-    body: {success: ture}
+    body: {success: true}
   }
+}
+
+function updateModal(props, slots){
+  return Modal({ name: "update-modal",$data: props }, [
+    ModalBody([
+      Row([
+        // preview
+        Col({},slots),
+        //properties
+        Col([
+          View([
+            Input({py: 0, mx: 0, label: "name", $model: 'this.name' }),
+            Input({py: 0, mx: 0, label: "description", "u-modal": 'description' }),
+            Input({py: 0, mx: 0, label: "url", name: "url" }),
+            Input({py: 0, mx: 0, label: "caption", name: "caption" }),
+            Input({py: 0, mx: 0, label: "width", name: "width" }),
+            Input({py: 0, mx: 0, label: "height", name: "height" }),
+            Input({py: 0, mx: 0, label: "alt", name: "alt" }),
+            Input({py: 0, mx: 0, label: "type", name: "type" }),
+          ]),
+          Row([
+            //control buttons
+            Button({ color: "primary", onClick: "$modal.close()" }, "Close"),
+            Button(
+              {
+                color: "error",
+                onClick: runAction(
+                  "update",
+                  `{id, name, description, url, caption, width, height, alt, type}`,
+                  "{$modal.close();loader = !loader}"
+                ),
+              },
+              "Update"
+            ),
+          ]),
+        ]),
+      ]),
+    ]),
+  ]);
 }
 
 function Media(props, slots){
@@ -115,9 +194,6 @@ function OffCanvas() {
       $effect: "loader; " + runAction("getAssets", `{type}`, "view = res.view"),
       style:
         "width: 300px; height: 100%; background-color: var(--color-base-800); color: var(--color-base-200);text-align:center",
-      script: function greating(View) {
-        return View("hellow");
-      },
     },
     [View("Assets"), headSection(), assetsSection()]
   );
@@ -178,36 +254,10 @@ function assetModal(name){
         CardBody([
           Button({color: 'primary', onClick: '$modal.close()'},'Close'),
           Button({color: 'error', onClick: runAction('remove', `{id: '${name.split('options-')[1]}'}`, 'loader = !loader' )},'Delete'),
-          Button({color: 'primary'},'Update'),
+          Button({color: 'primary', onClick:runAction ('getAsset',  `{id: '${name.split('options-')[1]}'}`,`{$modal.close(); view += res.view ; setTimeout(()=>$modal.open('update-modal'), 10)}`)},'Update'),
         ]),
       ])
     ])
   ])
-}
-
-function updateModal(name){
-  return Modal({name}, [
-    ModalBody([
-      Card({title: 'asset options', style: 'color: var(--color-base-800)'}, [
-        CardBody([
-          Col([
-          ]),
-          Col([
-            Input({label: 'name'}),
-            Input({label: 'url'}),
-            Input({label: 'alt'}),
-            Input({label: 'caption'}),
-            Input({label: 'description'}),
-            Input({label: 'pref-width'}),
-            Input({label: 'pref-height'}),
-          ]),
-        ]),
-      ])
-    ])
-  ])
-}
-
-function viewModal(){
-
 }
 
