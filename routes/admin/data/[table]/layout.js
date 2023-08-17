6b8342b5514fed4577ff6cd23f4c302c3f8d10f6
TheHadiAhmadi <thehadiahmadi@gmail.com>
2023-08-17 17:24:53 +0430

    update

--- routes/admin/data/[table]/layout.js
+++ routes/admin/data/[table]/layout.js
@@ -1,10 +1,8 @@
 export async function load({ ctx, params, locals }) {
-  const table = await ctx.Tables.query({
-    perPage: 100,
-    where: {
+  const table = await ctx.Tables.get({where: {
       slug: params.table
     }
-  }).then((res) => res.data[0]);
-
+  })
+  
   locals.table = table;
 }
