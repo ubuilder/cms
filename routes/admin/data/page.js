c13c404afad86f5b26f5bf5537d5cb7d0c6be2b6
EdrissAria <edrissalokozay77@gmail.com>
2023-08-20 03:41:28 -0400

    a little change

--- routes/admin/data/page.js
+++ routes/admin/data/page.js
@@ -1,2 +1,27 @@
 import { Button, Icon, Row } from "@ulibs/ui";
 import { Page } from "../../../components/Page.js";
+import { TableAddModal } from "./TableModals.js";
+import { TableItem } from "./TableItem.js";
+
+export * from "./actions.js";
+
+export async function load({ ctx }) {
+  const tables = await ctx.table("tables").query();
+  return {
+    tables: tables.data,
+  };
+}
+
+export default ({ tables }) =>
+  Page(
+    {
+      title: "Tables",
+      actions: [
+        Button(
+          { onClick: "$modal.open('table-add-modal')", color: "primary" },
+          [Icon({ name: "plus" }), "Create Table"]
+        ),
+      ],
+    },
+    [Row([tables.map((table) => TableItem(table))]), TableAddModal()]
+  );
